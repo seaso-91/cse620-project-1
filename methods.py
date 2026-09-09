2343ee80a4d13e1f342d6cb15ed02a0587d794fe
LiamS57 <william.seymour656@topper.wku.edu>
2026-09-09 15:11:24 -0400

    added initial automatic differentation to gradient descent with sympy (untested)

--- methods.py
+++ methods.py
@@ -1,6 +1,10 @@
 from typing import Tuple, Iterable
 import numpy as np
 
+from sympy import lambdify
+from sympy.core.symbol import Symbol
+from sympy.core.add import Add
+
 # Optimization methods implemented in this module
 
 TOLERANCE = 1e-6
@@ -10,35 +14,46 @@ MAX_ITER = 2000
 def newtons_method() -> Tuple[np.ndarray, np.ndarray]:
     ...
 
+
 def gradient_descent(
-        f_x: callable, 
-        df_dx: callable, 
-        x0: float, 
+        x_symbols: Tuple[Symbol],  # need to pass these for automatic differentation
+        f_symbolic: Add,  # sympy functions seem to be defined as an operation "tree", and most polynomials will have "Add" as the topmost op. TODO: make this better 
+        x0: Tuple[float],  # initial values for each symbol
         bounds: Iterable, 
         step_size: float,
         max_iter: int = MAX_ITER,
         eps: float = TOLERANCE
         ) -> Tuple[np.ndarray, np.ndarray]:
+
+    assert len(x_symbols) == len(x0)
+
+    # automatic differentation
+    grad_f_symbolic = [f_symbolic.diff(var) for var in x_symbols]  # first-order gradient vector
+    f_lambda = lambdify(x_symbols, f_symbolic, "numpy")
+    grad_f_lambda = [lambdify(x_symbols, grad, "numpy") for grad in grad_f_symbolic]
     
     x_history = [x0]
-    y_history = [f_x(x0)]
+    y_history = [f_lambda(*x0)]
 
-    for i in range(0, max_iter):
-        x_t = x_history[i] - (step_size * df_dx(x_history[i]))
-        x_t = min(max(bounds[0], x_t), bounds[1]) # Ensures that no movement occurs outside of the function bounds
-        y_t = f_x(x_t)
+    for i in range(max_iter):
+        x_t = np.zeros_like(x0)
+        for j in range(len(x0)):
+            x_t[j] = x_history[i][j] - (step_size * grad_f_lambda[j](*x_history[i]))
+            x_t[j] = min(max(bounds[j][0], x_t[j]), bounds[j][1])
+        y_t = f_lambda(*x_t)
 
         x_history.append(x_t)
         y_history.append(y_t)
 
-        if eps:
-            if np.abs(x_t - x_history[i]) < eps:
-                break
+        if np.abs(x_t - x_history[i]).sum() < eps:  # eps is always provided
+            break
 
     return x_history, y_history
 
+
 def adagrad() -> Tuple[np.ndarray, np.ndarray]:
     ...
+
 
 def adam() -> Tuple[np.ndarray, np.ndarray]:
     ...
