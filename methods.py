b8267a788ba979ddaa06f8034bc5a298cdfc9d9d
Jack Jones <jmjone37@louisville.edu>
2026-09-09 16:38:43 -0400

    added adam method

--- methods.py
+++ methods.py
@@ -68,5 +68,64 @@ def adagrad() -> Tuple[np.ndarray, np.ndarray]:
     ...
 
 
-def adam() -> Tuple[np.ndarray, np.ndarray]:
-    ...
+def adam(x_symbols: Tuple[Symbol],  # need to pass these for automatic differentation
+        f_symbolic: Add,  # sympy functions seem to be defined as an operation "tree", and most polynomials will have "Add" as the topmost op. TODO: make this better 
+        x0: np.ndarray,  # initial values for each symbol
+        bounds: Iterable, 
+        step_size: float,
+        beta1: float,
+        beta2: float,
+        max_iter: int = MAX_ITER,
+        eps: float = TOLERANCE
+        ) -> Tuple[list, list]:
+    '''Performs 'adam' optimization on a symbolic function.
+
+    Args:
+        x_symbols (Tuple[Symbol]): Symbols representing the variables of the function.
+        f_symbolic (Add): Symbolic representation of the function to be minimized.
+        x0 (np.ndarray[float]): Initial values for each symbol, given as a numpy array of floats (must have decimals).
+        bounds (Iterable): Bounds for each variable as (min, max) tuples.
+        step_size (float): Step size for the gradient descent updates.
+        beta1 (float): Exponential decay rate for the first moment estimates.
+        beta2 (float): Exponential decay rate for the second moment estimates.
+        max_iter (int, optional): Maximum number of iterations. Defaults to MAX_ITER.
+        eps (float, optional): Convergence tolerance. Defaults to TOLERANCE.
+
+    Returns:
+        Tuple[list, list]: History of variable values and corresponding function values.
+    '''
+    assert len(x_symbols) == len(x0)
+
+    # automatic differentation
+    grad_f_symbolic = [f_symbolic.diff(var) for var in x_symbols]  # first-order gradient vector
+    f_lambda = lambdify(x_symbols, f_symbolic, "numpy")
+    grad_f_lambda = [lambdify(x_symbols, grad, "numpy") for grad in grad_f_symbolic]
+    
+    x_history = [x0]
+    y_history = [f_lambda(*x0)]
+    m_history = [np.zeros_like(x0)]  # initialize first moment vector for Adam optimizer
+    v_history = [np.zeros_like(x0)]  # initialize second moment vector for Adam optimizer
+
+    for i in range(max_iter):
+        x_t = np.zeros_like(x0)
+        m_t = np.zeros_like(x0)
+        v_t = np.zeros_like(x0)
+        for j in range(len(x0)):
+            m_t[j] = beta1 * m_history[i][j] + (1 - beta1) * grad_f_lambda[j](*x_history[i])
+            v_t[j] = beta2 * v_history[i][j] + (1 - beta2) * (grad_f_lambda[j](*x_history[i]) ** 2)
+            m_t[j] = m_t[j] / (1 - beta1 ** (i + 1))
+            v_t[j] = v_t[j] / (1 - beta2 ** (i + 1))
+            x_t[j] = x_history[i][j] - step_size * m_t[j] / (np.sqrt(v_t[j]) + eps)
+            x_t[j] = min(max(bounds[j][0], x_t[j]), bounds[j][1])
+
+        y_t = f_lambda(*x_t)
+
+        x_history.append(x_t)
+        y_history.append(y_t)
+        m_history.append(m_t)
+        v_history.append(v_t)
+
+        if np.abs(x_t - x_history[i]).sum() < eps:  # eps is always provided
+            break
+
+    return x_history, y_history
