a4370455e971f108eb010e9d98f226b4d7f88b64
Jack Jones <jmjone37@louisville.edu>
2026-09-10 14:55:30 -0400

    added support for multiple paths

--- utilities.py
+++ utilities.py
@@ -5,22 +5,36 @@ from sympy.core.symbol import Symbol
 from sympy.core.add import Add
 from typing import Tuple
 
+colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k'] 
+
 def plot_contours_with_path(
     x_symbols: Tuple[Symbol],  # need to pass these for automatic differentation
     f_symbolic: Add,  # sympy functions seem to be defined as an operation "tree", and most polynomials will have "Add" as the topmost op. TODO: make this better 
-    results:   Tuple[list, list],
+    results:   Tuple[list, list] | list[Tuple[list, list]],
     xlim:   tuple[int, int] = None,
     ylim:   tuple[int, int] = None,
     levels: int = 40,
     title:  str = ""
     ):
 
-    P = np.array(results[0][:])
+    # convert results to a list to allow any number of paths to be plotted
+    if isinstance(results, Tuple):
+        P = [np.array(results[0][:])]
+    elif isinstance(results, list):
+        P=[]
+        for result in results:
+            P.append(np.array(result[0][:]))
+    else:
+        raise ValueError("Unsupported type for results")
 
+    min_x = min([p[:,0].min() for p in P])
+    max_x = max([p[:,0].max() for p in P])
+    min_y = min([p[:,1].min() for p in P])
+    max_y = max([p[:,1].max() for p in P])
     if xlim is None:
-        xlim = (min(P[:,0])-abs(min(P[:,0]))*0.25, max(P[:,0])+abs(max(P[:,0]))*0.25)
+        xlim = (min_x-abs(min_x)*0.25, max_x+abs(max_x)*0.25)
     if ylim is None:
-        ylim = (min(P[:,1])-abs(min(P[:,1]))*0.25, max(P[:,1])+abs(max(P[:,1]))*0.25)
+        ylim = (min_y-abs(min_y)*0.25, max_y+abs(max_y)*0.25)
 
     f_lambda = lambdify(x_symbols, f_symbolic)
     xs = np.linspace(xlim[0], xlim[1], 400)
@@ -33,7 +47,8 @@ def plot_contours_with_path(
     plt.figure()
     cs = plt.contour(X, Y, Z, levels=levels)
     plt.clabel(cs, inline=1, fontsize=8)
-    plt.plot(P[:,0], P[:,1], marker='o', linewidth=1)
+    for i, path in enumerate(P):
+        plt.plot(path[:,0], path[:,1], marker='o', linewidth=1, color=colors[i % len(colors)])
     plt.title(title)
     plt.xlabel('x'); plt.ylabel('y')
     plt.show()
